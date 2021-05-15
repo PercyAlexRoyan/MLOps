24607e6299f7910962f0f6de8a0403a58cc3a8aa
root <root@ip-172-31-17-149.ec2.internal>
2021-05-15 07:25:15 +0000

    y

--- train-lda.sh
+++ train-lda.sh
@@ -6,11 +6,8 @@ else
       echo " my-docker-lda not built"
 fi 
 			   
-if sudo grep "clf lda" train-lda.py
-then 
-      sudo docker run -p 5000:5000 my-docker-lda pyhton3 train-lda.py > result.txt
-	  sudo cat result.txt
-fi 
+sudo docker run -p 5000:5000 my-docker-lda pyhton3 train-lda.py > result.txt
+sudo cat result.txt 
 res=$(sudo cat result.txt)
 test=80
 if [ "$test" -gt "$res" ]	
