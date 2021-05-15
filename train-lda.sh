a24b1ca8fa4736e9b01a9c0c3aa7f653817ffbd3
Vyshnav VM <68809900+vyshnav-ox@users.noreply.github.com>
2021-05-15 12:22:55 +0530

    Update train-lda.sh

--- train-lda.sh
+++ train-lda.sh
@@ -11,7 +11,7 @@ then
       sudo docker run -p 5000:5000 my-docker-lda pyhton3 train-lda.py > result.txt
 	  sudo cat result.txt
 fi 
-res=${sudo cat result.txt}
+res=$(sudo cat result.txt)
 test=80
 if [ $test -gt $res ]	
 then 
